d14c3ae902c2da2a721d98e623bfa0806ec83edc
Sharp <rsha7868@740s-pl-127.local>
2017-10-23 13:39:49 -0600

    Second save.

--- src/model/Chatbot.java
+++ src/model/Chatbot.java
@@ -61,7 +61,17 @@ public class Chatbot
 	
 	public boolean lengthChecker(String input)
 	{
-		return false;
+		boolean validLength = false;
+		
+		if(input != null)
+		{
+			if(input.length() > 3)
+			{
+				validLength = true;		
+			}
+		}
+		
+		return validLength;
 	}
 	
 	public boolean htmlTagChecker(String input)
