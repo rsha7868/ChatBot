d0a22a70f2e6f5bde0e9ca90e20def0b0cddf227
Sharp <rsha7868@740s-pl-127.local>
2017-11-13 13:43:42 -0700

    Saving errors. :+1:

--- src/view/ChatFrame.java
+++ src/view/ChatFrame.java
@@ -1,6 +1,28 @@
 package view;
 
-public class ChatFrame
-{
+import chat.controllor.ChatbotController;
+import javax.swing.JFrame;
 
+
+
+public class ChatFrame extends JFrame
+{
+	private ChatbotController appController;
+	private Cahtbot appPane;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setResizable(false);
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
 }
