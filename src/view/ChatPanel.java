25e052a99a74606817278eca4608a846c1d82767
Sharp <rsha7868@740s-pl-127.local>
2017-11-15 12:47:26 -0700

    Working on work on the JPanel for chatbot.

---
+++ src/view/ChatPanel.java
@@ -0,0 +1,34 @@
+package view;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.JTextArea;
+import chat.controller.ChatController;
+import java.awt.Color;
+
+public class ChatPanel extends JPanel
+{
+	private ChatController appController;
+	private JButton chatButton;
+	private JTextField inputfield;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatController appController)
+	{
+		
+	}
+	public void setupPanel()
+	{
+		
+	}
+	public void setupLayout()
+	{
+		
+	}
+	public void setupListeners()
+	{
+		
+	}
+}
