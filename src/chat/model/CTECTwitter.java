4c75ce1e48092b8954e6116419f8ef45686384b6
Sharp <rsha7868@740s-pl-127.local>
2018-03-08 13:47:01 -0700

    SAving the  generateWordsCount and removeBanks method in twitter.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -157,4 +157,28 @@ public CTECTwitter(ChatbotController appController)
 			}
 		}
 	}
+	private void removeBlanks()
+	{
+		for(int index = tweetedWords.size() -1; index >= 0; index--)
+		{
+			if(tweetedWords.get(index).trim().length() == 0)
+			{
+				tweetedWords.remove(index);
+			}
+		}
+	}
+	private void generateWordCount()
+	{
+		for(String word : tweetedWords)
+		{
+			if(!wordsAndCount.containsKey(word.toLowerCase()))
+			{
+				wordsAndCount.put(word.toLowerCase(), 1);
+			}
+			else
+			{
+				wordsAndCount.replace(word.toLowerCase(), wordsAndCount.get(word.toLowerCase()) + 1);
+			}
+		}
+	}
 }
